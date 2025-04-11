54f5ed93611481602586dba33816d964f4706961
sixthkind <me@sixthkind.com>
2025-04-11 15:40:57 -0400

    added false pb autoCancellation

--- utils/pb.ts
+++ utils/pb.ts
@@ -2,4 +2,5 @@ import PocketBase from 'pocketbase';
 const config = useRuntimeConfig()
 const pocketbaseURL = String(config.public.pocketbaseURL);
 const pb = new PocketBase(pocketbaseURL);
+pb.autoCancellation(false);
 export { pb };
